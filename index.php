6b6a7100c83a2f4faffd7c826fdc82ea94c30556
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-06-25 16:32:30 +0200

    Refactor Movie class to include genre and update description method

--- index.php
+++ index.php
@@ -15,23 +15,35 @@
     {
         public string $title;
         public int $year;
+        public Genre $genre;
 
-        public function __construct($_title, $_year)
+        public function __construct($_title, $_year, Genre $_genre)
         {
 
             $this->title = $_title;
             $this->year = $_year;
+            $this->genre = $_genre;
         }
 
         public function descrivi()
         {
-            return "il film: $this->title è uscito nell'anno: $this->year";
+            return "il film: $this->title è uscito nell'anno: $this->year. Questo film è un genere: {$this->genre->movieGenre}";
         }
     }
 
-    $inception = new Movie("Inception", 2010);
+    class Genre
+    {
+        public string $movieGenre;
 
-    $interstellar = new Movie("Interstellar", 2014);
+        public function __construct($_movieGenre)
+        {
+            $this->movieGenre = $_movieGenre;
+        }
+    }
+
+    $inception = new Movie("Inception", 2010, new Genre("Azione"));
+
+    $interstellar = new Movie("Interstellar", 2014, new Genre("Fantascienza"));
 
     echo $inception->descrivi();
 
